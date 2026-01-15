7f8054b2cb0803789aafaf978c097c3c536ded72
Kitty <xinyili7@uw.edu>
2026-01-14 22:34:10 -0800

    save

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+
+// XIAO ESP32-C3 上 ADC 可用引脚
+// A0 = GPIO2
+#define ADC_PIN A0
+
+#define ADC_MAX 4095.0
+#define VREF 3.3
+
+void setup() {
+  Serial.begin(115200);
+  delay(300);
+
+  Serial.println("ESP32-C3 Voltage Reader Ready");
+  Serial.println("ADC    Voltage(V)");
+}
+
+void loop() {
+  int adcValue = analogRead(ADC_PIN);
+
+  float voltage = (adcValue / ADC_MAX) * VREF;
+
+  Serial.print(adcValue);
+  Serial.print("    ");
+  Serial.println(voltage, 3);
+
+  delay(300);
+}
